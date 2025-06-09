9d862c4b1e3ff8b4d8e09d680d511cf22da1df37
Rudra Narayan Das <123449668+Rudradas01@users.noreply.github.com>
2025-06-09 15:37:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,4 +25,5 @@ app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res)=> res.send("API is Working fine"))
 
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));

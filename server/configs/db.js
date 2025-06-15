ddbf5108f46b91de4d6c6816134f7782dc2c3091
Rudra Narayan Das <123449668+Rudradas01@users.noreply.github.com>
2025-06-15 14:43:55 +0530

    Update db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,18 +3,6 @@ import mongoose from "mongoose";
 let isConnected = false;
 
 const connectDB = async () => {
-<<<<<<< HEAD
-  if (isConnected) return;  
- 
-  try {
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true, 
-      connectTimeoutMS: 70000,
-      socketTimeoutMS: 70000,
-    });
-
-=======
   if (isConnected) return;
 
   try {
@@ -23,7 +11,6 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     });
 
->>>>>>> f4b7a28 (added)
     isConnected = true;
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
@@ -32,8 +19,4 @@ const connectDB = async () => {
   }
 };
 
-<<<<<<< HEAD
 export default connectDB;
-=======
-export default connectDB;
->>>>>>> f4b7a28 (added)
